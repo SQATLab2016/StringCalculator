a9583e70e8d90df3f59f3fff6b4b17ce6ebf9e5d
somename <someemail>
2016-09-14 15:20:41 +0300

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -25,6 +25,9 @@ public class StringCalculator {
 		while (prevIndex != -1) {
 			prevIndex = str.indexOf('\n', prevIndex);
 			
+			if (prevIndex != -1 && prevIndex < str.length())
+				prevIndex++;
+			
 			if (str.charAt(prevIndex) == ',')
 				throw new StringCalculatorException();
 		}
