59bb1de82d710ac11f7be80867c4e02cb78a3e58
somename <someemail>
2016-09-14 16:11:31 +0300

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,3 +1,4 @@
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
@@ -15,13 +16,14 @@ public class StringCalculator {
 			return Integer.parseInt(numbersStr);
 		}
 		
-		// Returns the sum of an unknown amount of numbers,
-		// separated by commas or newlines
+		// Newlines and commas are not allowed next to each other
 		Pattern p = Pattern.compile(",\\n|\\n,");
 		Matcher m = p.matcher(numbersStr);
 		if (m.find()) {
 			throw new StringCalculatorException();
 		}
+		// Returns the sum of an unknown amount of numbers,
+		// separated by commas or newlines
 		else if (numbersStr.matches("^\\d+[\\d,\\n]+\\d+$")) {
 			String numbers[] = numbersStr.split(",+|\\n+");
 			
@@ -33,6 +35,6 @@ public class StringCalculator {
 			return sum;
 		}
 		
-		return 0;
+		throw new StringCalculatorException();
 	}
 }
