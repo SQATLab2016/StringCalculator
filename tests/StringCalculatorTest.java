485f4238b81725b0d04d75b12ce91cad6c3bb0b9
somename <someemail>
2016-09-14 15:38:47 +0300

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -28,4 +28,16 @@ public class StringCalculatorTest {
 		assertEquals("One number (5)", 5, sum);
 	}
 
+	@Test
+	public void testStringCalculator_OneNumber_2() {
+		// Arrange
+		String numbers = "37";
+		
+		// Act
+		int sum = StringCalculator.add(numbers);
+		
+		// Assert
+		assertEquals("One number (37)", 37, sum);
+	}
+
 }
