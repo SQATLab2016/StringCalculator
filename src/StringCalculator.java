b8a335ec8f90d10e1f400c5599bde225841740a9
somename <someemail>
2016-09-14 15:31:44 +0300

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,6 +1,11 @@
 
 public class StringCalculator {
 	public int add(String numbersStr) {
+		// Returns 0 for an empty string
+		if (numbersStr == "") {
+			return 0;
+		}
+		
 		// Returns the sum of the numbers given in numbersStr
 		
 		// not yet implemented
