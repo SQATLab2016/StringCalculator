229fe4a4ed525fa1d4bacf631136c3baaeff208d
somename <someemail>
2016-09-15 16:26:29 +0300

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -5,6 +5,7 @@ public class StringCalculator {
 		int result = 0;
 		for (int i = 0; i < str.length(); i++) {
 			currentChar = str.substring(i, i+1);
+			if(isNumber(currentChar));
 		}
 		// not yet implemented
 		return result;
