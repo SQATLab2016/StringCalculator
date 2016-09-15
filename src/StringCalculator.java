9ba977c7530b9544a6100928d07f7449d0ac6b27
somename <someemail>
2016-09-15 16:28:04 +0300

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,9 +3,11 @@ public class StringCalculator {
 	public int add(String str) {
 		// Returns the sum of the numbers given in numbersStr
 		int result = 0;
-		for (int i = 0; i < str.length(); i++) {
-			currentChar = str.substring(i, i+1);
-			if(isNumber(currentChar));
+		if (str.equals(""))
+			return 0;
+		
+		if (str.length() == 1){
+			return (int)str;
 		}
 		// not yet implemented
 		return result;
