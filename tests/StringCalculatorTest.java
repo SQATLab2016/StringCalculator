ef2b362f009ee6df1c493ffbadcfff7f08fe9af2
somename <someemail>
2016-09-15 15:11:40 +0300

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,28 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStringWithNumber() {
+		String s = "1";
+		StringCalculator SC = new StringCalculator();
+		try {
+			assertEquals(0, SC.add(s));
+		}
+		catch (Exception e) {
+						
+		}
+	}
+	
+	@Test
+	public void testStringWithLetter() {
+		String s = "2";
+		StringCalculator SC = new StringCalculator();
+		try {
+			SC.add(s);
+		}
+		catch (Exception e) {
+			boolean thrown = true;
+			assertEquals(true, thrown);
+		}
 	}
 
 }
