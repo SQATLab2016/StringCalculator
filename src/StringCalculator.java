6c54f3c9511ba933fbed0a36ce3b256e2ac38041
somename <someemail>
2016-09-15 16:37:26 +0300

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -5,17 +5,7 @@ public class StringCalculator {
 		if (str.equals(""))
 			return 0;
 		
-		else if (str.length() == 1){
-			return Integer.parseInt(str);
-		}
-		
-		else if (str.length() == 3){
-			int num1 = Integer.parseInt(str.substring(0, 1));
-			int num2 = Integer.parseInt(str.substring(2, 3));
-			return num1 + num2;
-		}
-		else
-			return -1;
+		int length = str.length();
 		// not yet implemented
 	}
 }
