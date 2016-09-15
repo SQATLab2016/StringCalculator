e5980c1767d725778c8c7244edc7c4351d72b518
somename <someemail>
2016-09-15 15:16:09 +0300

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -25,7 +25,7 @@ public class StringCalculatorTest {
 			SC.add(s);
 		}
 		catch (Exception e) {
-			System.out.println(e.toString());
+			//System.out.println(e.toString());
 			thrown = true;
 		}
 		assertEquals("exception thrown", true, thrown);
