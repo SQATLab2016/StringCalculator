5096ae9a19d9e4c30f3d80a170b388cf76a20e0b
somename <someemail>
2016-09-15 15:04:48 +0300

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,24 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException{
+		if(checkIfStringContainsNumbers(numbersStr)) {
+			return 0;
+		}
+		else {
+			throw new StringCalculatorException("String contains more than just numbers");
+		}
 		// Returns the sum of the numbers given in numbersStr
 		
 		// not yet implemented
-		return 0;
+		
+	}
+	
+	private boolean checkIfStringContainsNumbers(String s) {
+		for(char c: s.toCharArray()) {
+			if(!Character.isDigit(c)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
