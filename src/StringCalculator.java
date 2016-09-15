a71433559bc162c75aa529a7fd082c456289b82d
somename <someemail>
2016-09-15 16:22:50 +0300

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -7,8 +7,10 @@ public class StringCalculator {
 		if(checkIfStringContainsOnlyNumbersAndAcceptableCharacters(numbersStr) && checkIfCharacterIsANumber(numbersStr.charAt(0))) {
 			for(char c: numbersStr.toCharArray()) {
 				int counter = 0;
-				if(counter%2 == 0 && checkIfCharacterIsANumber(c)) {
-					numbers.add(Character.getNumericValue(c));
+				if(counter%2 == 0) {
+					if (checkIfCharacterIsANumber(c)) {
+						numbers.add(Character.getNumericValue(c));
+					}
 				}
 				else {
 					throw new StringCalculatorException("String contains too many 'break' characters in a row");
