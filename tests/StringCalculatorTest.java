85d0bd9e15f007cf57fec482d23399984b9df8d8
Olli Hiekkaranta <olli@exove.fi>
2016-09-16 15:24:23 +0300

    add calculation result test

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -27,5 +27,40 @@ public class StringCalculatorTest {
 	public void StringCalculatorTest_invalid_numbers() throws StringCalculatorException  {
 		int result = calc.add("3,2");
 	}
+	
+	@Test
+	public void StringCalculatorTest_1_2_3() throws StringCalculatorException  {
+		int result = calc.add("1,2");
+		assertTrue("1 + 2 returns a wrong answer " + result, result == 3);
+	}
+	
+	@Test
+	public void StringCalculatorTest_1_2_2_5() throws StringCalculatorException  {
+		int result = calc.add("1,2,2");
+		assertTrue("1 + 2 + 2 returns a wrong answer " + result, result == 5);
+	}
+	
+	@Test
+	public void StringCalculatorTest_1_0_0_1() throws StringCalculatorException  {
+		int result = calc.add("1,0,0");
+		assertTrue("1 + 0 + 0 returns a wrong answer " + result, result == 1);
+	}
+	
+	@Test
+	public void StringCalculatorTest_0_0_0() throws StringCalculatorException  {
+		int result = calc.add("0,0");
+		assertTrue("0 + 0 returns a wrong answer " + result, result == 0);
+	}
+	
+	@Test
+	public void StringCalculatorTest_0() throws StringCalculatorException  {
+		int result = calc.add("0");
+		assertTrue("0 returns a wrong answer " + result, result == 0);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void StringCalculatorTest_negative() throws StringCalculatorException  {
+		int result = calc.add("-1,0");
+	}
 
 }
