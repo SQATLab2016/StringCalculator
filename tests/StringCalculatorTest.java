25e8211e92181373c1d27c6d04774b30b8f7d762
Olli Hiekkaranta <olli@exove.fi>
2016-09-16 14:42:40 +0300

    add tests for empty string returning 0

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,25 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
+	
+	StringCalculator calc = new StringCalculator();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void StringCalculatorTest_EmptyString() {
+		int result = calc.add("");
+		assertEquals("Empty string returns wrong value", 0, result);
+	}
+	
+	@Test
+	public void StringCalculatorTest_NonEmptyString() {
+		int result = calc.add("1,2");
+		assertTrue("Non Empty string returns wrong value", 0 != result);
+	}
+	
+	@Test
+	public void StringCalculatorTest_invalid_string() {
+		int result = calc.add("asdasd");
+		assertTrue("Non Empty string returns wrong value", 0 != result);
 	}
 
 }
