5f59581d4cd2dc4dd3e4d6ac0d9b6f6249560ebb
Olli Hiekkaranta <olli@exove.fi>
2016-09-16 14:49:52 +0300

    add converting string to integers and exception throwing

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,13 +2,36 @@
 public class StringCalculator {
 	
 	// Returns the sum of the numbers given in numbersStr
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException {
 		
 		if (numbersStr.length() == 0) {
 			return 0;
 		}
 		
+		int[] numbers = StringArrayToInt(numbersStr.split(","));
+		
 		return 1;
+	}
+	
+	private int[] StringArrayToInt(String[] strNumbers) throws StringCalculatorException {
+		int[] intNumbers = {};
+		int tempInt;
+		for (int i = 0; i < strNumbers.length; i++) {
+			tempInt = Integer.parseInt(strNumbers[i]);
+			if (validateNumber(tempInt)) {
+				intNumbers[i] = tempInt;
+			}
+			else {
+				throw (new StringCalculatorException());
+			}
+		}
+		
+		return intNumbers;
+	}
+	
+	private boolean validateNumber(int number) {
+		// TODO add logic
+		return true;
 	}
 
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -7,19 +7,19 @@ public class StringCalculatorTest {
 	StringCalculator calc = new StringCalculator();
 
 	@Test
-	public void StringCalculatorTest_EmptyString() {
+	public void StringCalculatorTest_EmptyString() throws StringCalculatorException  {
 		int result = calc.add("");
 		assertEquals("Empty string returns wrong value", 0, result);
 	}
 	
 	@Test
-	public void StringCalculatorTest_NonEmptyString() {
+	public void StringCalculatorTest_NonEmptyString() throws StringCalculatorException  {
 		int result = calc.add("1,2");
 		assertTrue("Non Empty string returns wrong value", 0 != result);
 	}
 	
 	@Test
-	public void StringCalculatorTest_invalid_string() {
+	public void StringCalculatorTest_invalid_string() throws StringCalculatorException {
 		int result = calc.add("asdasd");
 		assertTrue("Non Empty string returns wrong value", 0 != result);
 	}
