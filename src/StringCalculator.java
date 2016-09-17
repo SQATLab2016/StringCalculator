9465752317c3db7314a646466bf804514fe3fa59
kuzdogan <kaanuzdogan@hotmail.com>
2016-09-17 19:11:08 +0300

    Method for adding unknown amount of number

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,48 +1,54 @@
 
 public class StringCalculator {
-	public int add(String str) {
-		// Returns the sum of the numbers given in numbersStr
-		if (str.equals(""))
-			return 0;
-		int commaIndex = -1;
-		for (int i = 0; i < str.length(); i++) {
-			if(str.charAt(i) == 44)
-				commaIndex = i;
-		}
-		
-		// If no commas, i.e. only one number
-		if(commaIndex == -1)
-			return Integer.parseInt(str);
-		// Two numbers.
-		else{
-			String str1 = str.substring(0, commaIndex);
-			String str2 = str.substring(commaIndex+1);
-			int num1 = Integer.parseInt(str1);
-			int num2 = Integer.parseInt(str2);
-			return num1 + num2;
-		}
-		// not yet implemented
-	}
-	
-	
-	// TODO: Complete method
-	public int addUnknown(String str){
-		if(countCommas(str)<2)
-			return add(str);
-		else{
-			for (int i = 0; i < str.length(); i++) {
-				
-			}
-			return 0;
-		}
-	}
-	
-	public int countCommas(String str){
-		int counter = 0;
-		for (int i = 0; i < str.length(); i++) {
-			if(str.charAt(i) == ',')
-				counter++;
-		} 
-		return counter;
-	}
+  public int add(String str) {
+    // Returns the sum of the numbers given in numbersStr
+    if (str.equals(""))
+      return 0;
+    int commaIndex = -1;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == 44)
+        commaIndex = i;
+    }
+
+    // If no commas, i.e. only one number
+    if (commaIndex == -1)
+      return Integer.parseInt(str);
+    // Two numbers.
+    else {
+      String str1 = str.substring(0, commaIndex);
+      String str2 = str.substring(commaIndex + 1);
+      int num1 = Integer.parseInt(str1);
+      int num2 = Integer.parseInt(str2);
+      return num1 + num2;
+    }
+  }
+
+  // TODO: Complete method
+  public int addUnknown(String str) {
+    int commaCount = countCommas(str);
+    int result = 0;
+    if (commaCount < 2)
+      return add(str);
+    else {
+      String numberStr = "";
+      for (int i = 0; i < str.length(); i++) {
+        String current = str.substring(i,i+1);
+        if(current != ",")
+          numberStr += current;
+        else{
+          result += Integer.parseInt(numberStr);
+        }
+      }
+      return result;
+    }
+  }
+
+  public int countCommas(String str) {
+    int counter = 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == ',')
+        counter++;
+    }
+    return counter;
+  }
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,48 +4,72 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
-	@Test
-	public void testAddWithEmpty() {
-		StringCalculator calc = new StringCalculator();
-		
-		String test="";
-		
-		assertEquals(0, calc.add(test));
-	}
-	
-	@Test
-	public void testAddWithOne() {
-		StringCalculator calc = new StringCalculator();
-		
-		String test="1235";
-		
-		assertEquals(1235, calc.add(test));
-	}
-	
-	@Test
-	public void testAddWithTwo() {
-		StringCalculator calc = new StringCalculator();
-		
-		String test="213,500";
-		
-		assertEquals(713, calc.add(test));
-	}
-	
-	@Test
-	public void testCountCommasWith1() {
-		StringCalculator calc = new StringCalculator();
-		
-		String test="213,500";
-		
-		assertEquals(1, calc.countCommas(test));
-	}
-	
-	@Test
-	public void testCountCommasWith4() {
-		StringCalculator calc = new StringCalculator();
-		
-		String test="213,500,2,0,1132";
-		
-		assertEquals(4, calc.countCommas(test));
-	}
+  @Test
+  public void testAddWithEmpty() {
+    StringCalculator calc = new StringCalculator();
+
+    String test = "";
+
+    assertEquals(0, calc.add(test));
+  }
+
+  @Test
+  public void testAddWithOne() {
+    StringCalculator calc = new StringCalculator();
+
+    String test = "1235";
+
+    assertEquals(1235, calc.add(test));
+  }
+
+  @Test
+  public void testAddWithTwo() {
+    StringCalculator calc = new StringCalculator();
+
+    String test = "213,500";
+
+    assertEquals(713, calc.add(test));
+  }
+
+  @Test
+  public void testCountCommasWith1() {
+    StringCalculator calc = new StringCalculator();
+
+    String test = "213,500";
+
+    assertEquals(1, calc.countCommas(test));
+  }
+
+  @Test
+  public void testCountCommasWith4() {
+    StringCalculator calc = new StringCalculator();
+
+    String test = "213,500,2,0,1132";
+
+    assertEquals(4, calc.countCommas(test));
+  }
+
+  public void testAddUnknownWith5() {
+    StringCalculator calc = new StringCalculator();
+
+    String test = "213,500,2,45,3,10,2";
+
+    assertEquals(775, calc.addUnknown(test));
+  }
+  
+  public void testAddUnknownWith0() {
+    StringCalculator calc = new StringCalculator();
+
+    String test = "";
+
+    assertEquals(0, calc.addUnknown(test));
+  }
+  
+  public void testAddUnknownWith1() {
+    StringCalculator calc = new StringCalculator();
+
+    String test = "21313123";
+
+    assertEquals(21313123, calc.addUnknown(test));
+  }
 }
